736fb0fc80baac165c91f42139c4742e8151eef9
2wo5ive <70109871+2wo5ive@users.noreply.github.com>
2026-05-06 03:09:34 +0800

    admin fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main style={{ padding: "30px", fontFamily: "Arial" }}>
       <h1>Трекер доставки автомобиля</h1>
+      <p>Главная страница</p>
 
       <MapWrapper />
     </main>

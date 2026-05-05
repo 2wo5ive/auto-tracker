b744d8d390a40191b0e58cd6d5bd26057b3d2be7
2wo5ive <70109871+2wo5ive@users.noreply.github.com>
2026-05-06 03:24:31 +0800

    admin fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import MapWrapper from "../components/MapWrapper";
 export default function Home() {
   return (
     <main style={{ padding: "30px", fontFamily: "Arial" }}>
-      <h1>Трекер доставки автомобиля</h1>
-      <p>Главная страница</p>
+      <h1>Трекер доставки</h1>
 
       <MapWrapper />
     </main>
